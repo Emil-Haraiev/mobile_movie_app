9778b4859175d69cf8a4c4105b4feae8c210c372
Emil <e.garaev.dg@gmail.com>
2026-01-22 11:40:43 +0200

    Current clean version

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import {Text, View} from "react-native";
+
+export default function Index() {
+    return (
+        <View className='items-center mt-20'>
+            <Text className='mt-20 text-2xl '>Welcome</Text>
+        </View>
+    );
+}
